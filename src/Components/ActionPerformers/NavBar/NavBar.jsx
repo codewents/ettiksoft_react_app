9fcd8b290195210d18fe712c9083e03e29465979
Thirunavukarasu M <codewents@gmail.com>
2024-02-26 19:56:39 +0530

    Added ContactUs page.

--- src/Components/ActionPerformers/NavBar/NavBar.jsx
+++ src/Components/ActionPerformers/NavBar/NavBar.jsx
@@ -88,21 +88,21 @@ const NavBar = () => {
                   <div class="dropdown-cards d-flex flex-row flex-wrap">
                     <div class="col-6 col-md-4 my-3 ">
                       <div class="card card-hover border-0 border-radius" >
-                      <Link to='/iot-solutions'>
-                        <div class="card-body">
-                          <h5 class="card-title">IOT Solutions</h5>
-                          <p class="card-text">Description for Product 1.</p>
-                        </div>
+                        <Link to='/iot-solutions'>
+                          <div class="card-body">
+                            <h5 class="card-title">IOT Solutions</h5>
+                            <p class="card-text">Description for Product 1.</p>
+                          </div>
                         </Link>
                       </div>
                     </div>
                     <div class="col-6 col-md-4 my-3  ">
                       <div class="card card-hover border-0 border-radius">
-                      <Link to='/mobility-engineering'>
-                        <div class="card-body">
-                          <h5 class="card-title">Mobility Engineering</h5>
-                          <p class="card-text">Description for Product 2.</p>
-                        </div>
+                        <Link to='/mobility-engineering'>
+                          <div class="card-body">
+                            <h5 class="card-title">Mobility Engineering</h5>
+                            <p class="card-text">Description for Product 2.</p>
+                          </div>
                         </Link>
                       </div>
                     </div>
@@ -115,45 +115,45 @@ const NavBar = () => {
                   <h5 class="dropdown-item" >Company</h5>
                   <span class="dropdown-line"></span>
                   <div class="dropdown-cards d-flex flex-row flex-wrap">
-                    
-                      <div class="col-6 col-md-4 my-3 ">
-                        <div class="card card-hover border-0">
+
+                    <div class="col-6 col-md-4 my-3 ">
+                      <div class="card card-hover border-0">
                         <Link to='/about-us'>
                           <div class="card-body">
                             <h5 class="card-title">About Us</h5>
                             <p class="card-text d-none d-md-block">Description for Product 1.</p>
                           </div>
-                          </Link>
-                        </div>
+                        </Link>
                       </div>
-                    
+                    </div>
+
                     <div class="col-6 col-md-4 my-3 border-0">
                       <div class="card card-hover border-0">
-                      <Link to='/life-at-ettiksoft'>
-                        <div class="card-body">
-                          <h5 class="card-title">Life at Ettiksoft</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 2.</p>
-                        </div>
+                        <Link to='/life-at-ettiksoft'>
+                          <div class="card-body">
+                            <h5 class="card-title">Life at Ettiksoft</h5>
+                            <p class="card-text d-none d-md-block">Description for Product 2.</p>
+                          </div>
                         </Link>
                       </div>
                     </div>
                     <div class="col-6 col-md-4 my-3 ">
                       <div class="card card-hover border-0">
-                      <Link to='/careers'>
-                        <div class="card-body">
-                          <h5 class="card-title ">Careers</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 3.</p>
-                        </div>
+                        <Link to='/careers'>
+                          <div class="card-body">
+                            <h5 class="card-title ">Careers</h5>
+                            <p class="card-text d-none d-md-block">Description for Product 3.</p>
+                          </div>
                         </Link>
                       </div>
                     </div>
                     <div class="col-6 col-md-4 my-3 ">
                       <div class="card card-hover border-0">
-                      <Link to='/contact-us'>
-                        <div class="card-body">
-                          <h5 class="card-title">Contact us</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 4.</p>
-                        </div>
+                        <Link to='/contact-us'>
+                          <div class="card-body">
+                            <h5 class="card-title">Contact us</h5>
+                            <p class="card-text d-none d-md-block">Description for Product 4.</p>
+                          </div>
                         </Link>
                       </div>
                     </div>
@@ -173,176 +173,16 @@ const NavBar = () => {
                 </div>
               </li>
               <li class="nav-item">
-                <button type="button" class="btn btn-primary"><h6>Login</h6></button>
-              </li>
-
-            </ul>
-          </div>
-        </nav>
-      </div>
-      <div class="navbar-component">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <Link class="navbar-brand" to="/">Your Logo</Link>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <Link class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</Link>
-                <div class="dropdown-menu full-width" aria-labelledby="navbarDropdown">
-                  <Link class="dropdown-item" to="/products">Products</Link>
-                  <span class="dropdown-line"></span>
-                  <div class="dropdown-cards d-flex flex-row flex-wrap">
-                    <div class="col-12 col-md-4 my-3 ">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 1</h5>
-                          <p class="card-text">Description for Product 1.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3 border-0">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 2</h5>
-                          <p class="card-text">Description for Product 2.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3 ">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 3</h5>
-                          <p class="card-text">Description for Product 3.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3 ">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 4</h5>
-                          <p class="card-text">Description for Product 4.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 5</h5>
-                          <p class="card-text">Description for Product 5.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-
-
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</Link>
-                <div class="dropdown-menu full-width" aria-labelledby="navbarDropdown">
-                  <Link class="dropdown-item" href="#">Services</Link>
-                  <span class="dropdown-line"></span>
-                  <div class="dropdown-cards d-flex flex-row flex-wrap">
-                    <div class="col-12 col-md-4 my-3 ">
-                      <Link to='/iot-solutions'>
-                        <div class="card card-hover border-0">
-                          <div class="card-body">
-                            <h5 class="card-title">IOT Solutions</h5>
-                            <p class="card-text text-black">Description for Product 1.</p>
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                    <div class="col-12 col-md-4 my-3 border-0">
-                      <Link to='/mobility-engineering'>
-                        <div class="card card-hover border-0">
-                          <div class="card-body">
-                            <h5 class="card-title">Mobility Engineering Services</h5>
-                            <p class="card-text text-black">Description for Product 2.</p>
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Company</Link>
-                <div class="dropdown-menu full-width" aria-labelledby="navbarDropdown">
-                  <Link class="dropdown-item" href="#">Company</Link>
-                  <span class="dropdown-line"></span>
-                  <div class="dropdown-cards d-flex flex-row flex-wrap">
-                    <div class="col-12 col-md-4 my-3 ">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 1</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 1.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3 border-0">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 2</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 2.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3 ">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title ">Product 3</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 3.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3 ">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 4</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 4.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-12 col-md-4 my-3">
-                      <div class="card card-hover border-0">
-                        <div class="card-body">
-                          <h5 class="card-title">Product 5</h5>
-                          <p class="card-text d-none d-md-block">Description for Product 5.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item dropdown">
-                <Link class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Language
-                </Link>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <Link class="dropdown-item" href="#">EN</Link>
-                  <Link class="dropdown-item" href="#">GE</Link>
-                </div>
-              </li>
-              <li class="nav-item">
-                {/* <Link className="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Login</Link> */}
                 <EttiksoftButton key={1} buttonText={'Login'} buttonType={'bootstrap'} />
               </li>
+
             </ul>
           </div>
-
         </nav>
         {/* <PopupModal type={"Signin"} /> */}
         <PopupModal type={"Enquiry"} />
-
-
       </div>
+     
     </>
 
 
--- src/Pages/CompanyPages/ContactUsPage/ContactUsPage.jsx
+++ src/Pages/CompanyPages/ContactUsPage/ContactUsPage.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
+import image1 from '../../../Assets/images/contact-us/contact_section1.svg';
+import EttiksoftInput from '../../../Components/DataProviders/EttiksoftInput';
+import './ContactUsPage.scss'
+import { Link } from 'react-router-dom';
 
+import TrustedByGlobalBrands from '../../../Components/ActionPerformers/TrustedByGlobalBrands/TrustedByGlobalBrands'
 const ContactUsPage = () => {
   return (
-    <div>
-      <h1>
-        Contact US
-      </h1>
+    <div className="contact-us-container">
+      <div className="part-one d-flex justify-content-evenly align-items-center mt-5">
+        <div className="contact-us-content d-flex flex-column">
+          <h1>
+            Contact us
+          </h1>
+          <p className='text-justify'>
+            Having any questions about our products and services or looking for sales support. Our team is happy to answer your questions. Fill out the form, and we’ll be in touch as soon as possible.
+          </p>
+          <img src={image1} alt="" srcset="" className='contact-us-grp-img' />
+        </div>
+        <div className="contact-us-form bg-dark p-5">
+          <form>
+            <div className="d-flex align-items-center" style={{ 'gap': '1rem' }}>
+              <EttiksoftInput type={'text'} label={'First Name*'} />
+              <EttiksoftInput type={'text'} label={'Last Name*'} />
+            </div>
+            <EttiksoftInput type={'text'} label={'Business Email'} />
+            <EttiksoftInput type={'text'} label={'Business Name'} />
+            <div className="d-flex" style={{ 'gap': '1rem' }}>
+              <EttiksoftInput type={'text'} label={'ext'} />
+              <EttiksoftInput type={'text'} label={'Contact'} />
+            </div>
+            <EttiksoftInput type={'text'} label={'Product'} />
+            <EttiksoftInput type={'text'} label={'Type of Query'} />
+            <EttiksoftInput type={'text'} label={'Your Message'} />
+          </form>
+        </div>
+      </div>
+      {/* <div className="part-two mt-5 mb-3 bg-dark">
+        <div className="info-card d-flex p-5">
+          <div className="product-support">
+            <img src={image1} alt="" srcset="" className='contact-us-grp-img' height={100} width={100}/>
+            <h1>Product Support</h1>
+            <p>Looking for a demo, or have questions about our products and pricing?</p>
+            <Link>sales@ettiksoft.com</Link>
+          </div>
+          <hr />
+          <div className="product-support">
+            <img src={image1} alt="" srcset="" className='contact-us-grp-img' height={100} width={100}/>
+            <h1>Product Support</h1>
+            <p>Looking for a demo, or have questions about our products and pricing?</p>
+            <Link>sales@ettiksoft.com</Link>
+          </div>
+          <hr />
+          <div className="product-support">
+            <img src={image1} alt="" srcset="" className='contact-us-grp-img' height={100} width={100}/>
+            <h1>Product Support</h1>
+            <p>Looking for a demo, or have questions about our products and pricing?</p>
+            <Link>sales@ettiksoft.com</Link>
+          </div>
+        </div>
+      </div> */}
+      <TrustedByGlobalBrands/>
     </div>
   )
 }
