41aa3ae5b3a65dd4a7570d7c2a945e1bdc505d01
nandhan-cw <nandhan.codewents@gmail.com>
2024-02-26 11:15:43 +0530

    footer added

--- src/Components/ActionPerformers/Footer/Footer.jsx
+++ src/Components/ActionPerformers/Footer/Footer.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
-import './Footer.scss'
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import React, { useState } from 'react';
+import './Footer.scss';
 
-export default Footer
+const Footer = () => {
+  const [isOpenCompany, setIsOpenCompany] = useState(false);
+  const [isOpenProducts, setIsOpenProducts] = useState(false);
+  const [isOpenServices, setIsOpenServices] = useState(false);
+  const [isOpenContact, setIsOpenContact] = useState(false);
+
+  const toggleAccordion = (section) => {
+    if (window.innerWidth >= 568) return; // Only toggle under 568px
+
+    switch (section) {
+      case 'Company':
+        setIsOpenCompany(prevState => !prevState);
+        setIsOpenProducts(false);
+        setIsOpenServices(false);
+        setIsOpenContact(false);
+        break;
+      case 'Products':
+        setIsOpenProducts(prevState => !prevState);
+        setIsOpenCompany(false);
+        setIsOpenServices(false);
+        setIsOpenContact(false);
+        break;
+      case 'Services':
+        setIsOpenServices(prevState => !prevState);
+        setIsOpenCompany(false);
+        setIsOpenProducts(false);
+        setIsOpenContact(false);
+        break;
+      case 'Contact':
+        setIsOpenContact(prevState => !prevState);
+        setIsOpenCompany(false);
+        setIsOpenProducts(false);
+        setIsOpenServices(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <footer className="bg-dark text-light">
+      <div className="container py-4">
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-4">
+            <h5 className="mb-3" onClick={() => toggleAccordion('Logo')}>Logo</h5>
+            <ul className={`list-unstyled d-flex`}>
+              <li className="mr-3"><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+              <li className="mr-3"><a href="#"><i className="fab fa-twitter"></i></a></li>
+              <li className="mr-3"><a href="#"><i className="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-4">
+            <h5 className="mb-3" onClick={() => toggleAccordion('Company')}>Company</h5>
+            <ul className={`list-unstyled ${isOpenCompany ? 'visible' : ''}`}>
+              <li>About Us</li>
+              <li>Life at Ettiksoft</li>
+              <li>Careers</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-4">
+            <h5 className="mb-3" onClick={() => toggleAccordion('Products')}>Products</h5>
+            <ul className={`list-unstyled ${isOpenProducts ? 'visible' : ''}`}>
+              <li>Load Box</li>
+              <li>Break Out Box</li>
+              <li>EMI Wiring</li>
+              <li>EV Components</li>
+              <li>Variant Switch</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-4">
+            <h5 className="mb-3" onClick={() => toggleAccordion('Services')}>Services</h5>
+            <ul className={`list-unstyled ${isOpenServices ? 'visible' : ''}`}>
+              <li>IOT Solutions</li>
+              <li>Mobility Engineering</li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 mb-4">
+            <h5 className="mb-3" onClick={() => toggleAccordion('Contact')}>Contact</h5>
+            <ul className={`list-unstyled ${isOpenContact ? 'visible' : ''}`}>
+              <li>Phone</li>
+              <li>+91 73736 89304</li>
+              <li>+91 97898 81190</li>
+              <li>Email</li>
+              <li>info@ettiksoft.com</li>
+              <li>Head Office</li>
+              <li>No:6/1116-1/3, Paramathi Road, C E B Colony, Namakkal, Tamil Nadu 637001.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Components/ActionPerformers/NavBar/NavBar.jsx
+++ src/Components/ActionPerformers/NavBar/NavBar.jsx
@@ -1,58 +1,70 @@
-import React from 'react'
 import './NavBar.scss'
+import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import logo from '../../../Assets/images/ettiksoft-logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown,faGlobe } from '@fortawesome/free-solid-svg-icons'
 const NavBar = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState('EN');
+
+  const handleLanguageChange = (language) => {
+    setSelectedLanguage(language);
+  };
   return (
-    <div class="navbar-component">
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Your Logo</a>
+  <div class="navbar-component">
+   <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="#">
+        <img src={logo} alt="Logo"/>
+      </a>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
+      <ul class="navbar-nav ">
         <li class="nav-item">
-          <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
+          <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h6>Products</h6><FontAwesomeIcon icon={faAngleDown} /></a>
           <div class="dropdown-menu full-width" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Products</a>
+            <h5  class="dropdown-item" >Products</h5>
             <span class="dropdown-line"></span>
             <div class="dropdown-cards d-flex flex-row flex-wrap">
-                <div class="col-12 col-md-4 my-3 ">
+                <div class="col-6 col-md-4 my-3 ">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 1</h5>
+                      <h5 class="card-title">Load Box</h5>
                       <p class="card-text">Description for Product 1.</p>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-4 my-3 border-0">
+                <div class="col-6 col-md-4 my-3 border-0">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 2</h5>
+                      <h5 class="card-title">Break Out Box</h5>
                       <p class="card-text">Description for Product 2.</p>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-4 my-3 ">
+                <div class="col-6 col-md-4 my-3 ">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 3</h5>
+                      <h5 class="card-title">EMI Wiring</h5>
                       <p class="card-text">Description for Product 3.</p>
                     </div>
                   </div>
                 </div> 
-                <div class="col-12 col-md-4 my-3 ">
+                <div class="col-6 col-md-4 my-3 ">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 4</h5>
+                      <h5 class="card-title">EV Components</h5>
                       <p class="card-text">Description for Product 4.</p>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-4 my-3">
+                <div class="col-6 col-md-4 my-3">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 5</h5>
+                      <h5 class="card-title">Variant Switch</h5>
                       <p class="card-text">Description for Product 5.</p>
                     </div>
                   </div>
@@ -64,48 +76,24 @@ const NavBar = () => {
 
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
+        <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h6>Services</h6><FontAwesomeIcon icon={faAngleDown} /></a>
           <div class="dropdown-menu full-width" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Services</a>
+            <h5 class="dropdown-item">Services</h5>
             <span class="dropdown-line"></span>
             <div class="dropdown-cards d-flex flex-row flex-wrap">
-                <div class="col-12 col-md-4 my-3 ">
-                  <div class="card card-hover border-0">
+                <div class="col-6 col-md-4 my-3 ">
+                  <div class="card card-hover border-0 border-radius" >
                     <div class="card-body">
-                      <h5 class="card-title">Product 1</h5>
+                      <h5 class="card-title">IOT Solutions</h5>
                       <p class="card-text">Description for Product 1.</p>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-4 my-3 border-0">
-                  <div class="card card-hover border-0">
+                <div class="col-6 col-md-4 my-3  ">
+                  <div class="card card-hover border-0 border-radius">
                     <div class="card-body">
-                      <h5 class="card-title">Product 2</h5>
+                      <h5 class="card-title">Mobility Engineering</h5>
                       <p class="card-text">Description for Product 2.</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-12 col-md-4 my-3 ">
-                  <div class="card card-hover border-0">
-                    <div class="card-body">
-                      <h5 class="card-title">Product 3</h5>
-                      <p class="card-text">Description for Product 3.</p>
-                    </div>
-                  </div>
-                </div> 
-                <div class="col-12 col-md-4 my-3 ">
-                  <div class="card card-hover border-0">
-                    <div class="card-body">
-                      <h5 class="card-title">Product 4</h5>
-                      <p class="card-text">Description for Product 4.</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-12 col-md-4 my-3">
-                  <div class="card card-hover border-0">
-                    <div class="card-body">
-                      <h5 class="card-title">Product 5</h5>
-                      <p class="card-text">Description for Product 5.</p>
                     </div>
                   </div>
                 </div>
@@ -113,68 +101,62 @@ const NavBar = () => {
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Company</a>
+          <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h6>Company</h6><FontAwesomeIcon icon={faAngleDown} /></a>
           <div class="dropdown-menu full-width" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Company</a>
+            <h5 class="dropdown-item" >Company</h5>
             <span class="dropdown-line"></span>
             <div class="dropdown-cards d-flex flex-row flex-wrap">
-                <div class="col-12 col-md-4 my-3 ">
+                <div class="col-6 col-md-4 my-3 ">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 1</h5>
+                      <h5 class="card-title">About Us</h5>
                       <p class="card-text d-none d-md-block">Description for Product 1.</p>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-4 my-3 border-0">
+                <div class="col-6 col-md-4 my-3 border-0">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 2</h5>
+                      <h5 class="card-title">Life at Ettiksoft</h5>
                       <p class="card-text d-none d-md-block">Description for Product 2.</p>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-4 my-3 ">
+                <div class="col-6 col-md-4 my-3 ">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title ">Product 3</h5>
+                      <h5 class="card-title ">Careers</h5>
                       <p class="card-text d-none d-md-block">Description for Product 3.</p>
                     </div>
                   </div>
                 </div> 
-                <div class="col-12 col-md-4 my-3 ">
+                <div class="col-6 col-md-4 my-3 ">
                   <div class="card card-hover border-0">
                     <div class="card-body">
-                      <h5 class="card-title">Product 4</h5>
+                      <h5 class="card-title">Contact us</h5>
                       <p class="card-text d-none d-md-block">Description for Product 4.</p>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-4 my-3">
-                  <div class="card card-hover border-0">
-                    <div class="card-body">
-                      <h5 class="card-title">Product 5</h5>
-                      <p class="card-text d-none d-md-block">Description for Product 5.</p>
-                    </div>
-                  </div>
-                </div>
+             
             </div>
           </div>
         </li>
       </ul>
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown language">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Language
+          <FontAwesomeIcon class="icon" icon={ faGlobe } />   {selectedLanguage}
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">EN</a>
-            <a class="dropdown-item" href="#">GE</a>
+            <a class="dropdown-item" href="#" onClick={() => handleLanguageChange('EN')}>EN</a>
+            <a class="dropdown-item" href="#" onClick={() => handleLanguageChange('GE')}>GE</a>
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
+  <button type="button" class="btn btn-primary"><h6>Login</h6></button>
+</li>
+
       </ul>
     </div>
   </nav>
