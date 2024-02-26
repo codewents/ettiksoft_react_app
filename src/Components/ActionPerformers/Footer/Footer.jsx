caa29c675bb18caffa6a3414a1a08c0a7ac93259
nandhan-cw <nandhan.codewents@gmail.com>
2024-02-26 14:12:45 +0530

    footer accordian added

--- src/Components/ActionPerformers/Footer/Footer.jsx
+++ src/Components/ActionPerformers/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Footer.scss';
-
+import logo from '../../../Assets/images/ettiksoft-logo.png'
+import { faFacebook, faInstagram, faYoutube, faLinkedinIn } from '@fortawesome/free-solid-svg-icons'
 const Footer = () => {
   const [isOpenCompany, setIsOpenCompany] = useState(false);
   const [isOpenProducts, setIsOpenProducts] = useState(false);
@@ -41,59 +42,61 @@ const Footer = () => {
   };
 
   return (
-    <footer className="bg-dark text-light">
-      <div className="container py-4">
-        <div className="row">
-          <div className="col-md-3 col-sm-6 mb-4">
-            <h5 className="mb-3" onClick={() => toggleAccordion('Logo')}>Logo</h5>
-            <ul className={`list-unstyled d-flex`}>
-              <li className="mr-3"><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-              <li className="mr-3"><a href="#"><i className="fab fa-twitter"></i></a></li>
-              <li className="mr-3"><a href="#"><i className="fab fa-instagram"></i></a></li>
-            </ul>
-          </div>
-          <div className="col-md-3 col-sm-6 mb-4">
-            <h5 className="mb-3" onClick={() => toggleAccordion('Company')}>Company</h5>
-            <ul className={`list-unstyled ${isOpenCompany ? 'visible' : ''}`}>
-              <li>About Us</li>
-              <li>Life at Ettiksoft</li>
-              <li>Careers</li>
-              <li>Contact us</li>
-            </ul>
-          </div>
-          <div className="col-md-3 col-sm-6 mb-4">
-            <h5 className="mb-3" onClick={() => toggleAccordion('Products')}>Products</h5>
-            <ul className={`list-unstyled ${isOpenProducts ? 'visible' : ''}`}>
-              <li>Load Box</li>
-              <li>Break Out Box</li>
-              <li>EMI Wiring</li>
-              <li>EV Components</li>
-              <li>Variant Switch</li>
-            </ul>
-          </div>
-          <div className="col-md-3 col-sm-6 mb-4">
-            <h5 className="mb-3" onClick={() => toggleAccordion('Services')}>Services</h5>
-            <ul className={`list-unstyled ${isOpenServices ? 'visible' : ''}`}>
-              <li>IOT Solutions</li>
-              <li>Mobility Engineering</li>
-            </ul>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 mb-4">
-            <h5 className="mb-3" onClick={() => toggleAccordion('Contact')}>Contact</h5>
-            <ul className={`list-unstyled ${isOpenContact ? 'visible' : ''}`}>
-              <li>Phone</li>
-              <li>+91 73736 89304</li>
-              <li>+91 97898 81190</li>
-              <li>Email</li>
-              <li>info@ettiksoft.com</li>
-              <li>Head Office</li>
-              <li>No:6/1116-1/3, Paramathi Road, C E B Colony, Namakkal, Tamil Nadu 637001.</li>
-            </ul>
-          </div>
-        </div>
+    <footer >
+      <div  class="container">
+        <div class="row">
+            <div className="col-md-2 col-sm-6 my-4 mx-3">
+              <h5 className="mb-3" onClick={() => toggleAccordion('Logo')}>   <a class="navbar-brand" href="#">
+        <img src={logo} alt="Logo"/>
+      </a></h5>
+              <ul className={`list-unstyled d-flex`}>
+                <li className="mr-3"><a href="#"><FontAwesomeIcon class="icon" icon={ faFacebookF } /> </a></li>
+                <li className="mr-3"><a href="#"><FontAwesomeIcon class="icon" icon={ faInstagram } /></a> </li>
+                <li className="mr-3"><a href="#"><FontAwesomeIcon class="icon" icon={ faLinkedinIn } /> </a></li>
+                <li className="mr-3"><a href="#"><FontAwesomeIcon class="icon" icon={ faYoutube } /> </a></li>
+              </ul>
+            </div>
+            <div className="col-md-2 col-sm-6 mb-4 mx-3">
+              <h5 className="mb-3" onClick={() => toggleAccordion('Company')}>Company</h5>
+              <ul className={`list-unstyled ${isOpenCompany ? 'visible' : ''}`}>
+                <li>About Us</li>
+                <li>Life at Ettiksoft</li>
+                <li>Careers</li>
+                <li>Contact us</li>
+              </ul>
+            </div>
+            <div className="col-md-2 col-sm-6 mb-4 mx-3">
+              <h5 className="mb-3" onClick={() => toggleAccordion('Products')}>Products</h5>
+              <ul className={`list-unstyled ${isOpenProducts ? 'visible' : ''}`}>
+                <li>Load Box</li>
+                <li>Break Out Box</li>
+                <li>EMI Wiring</li>
+                <li>EV Components</li>
+                <li>Variant Switch</li>
+              </ul>
+            </div>
+            <div className="col-md-2 col-sm-6 mb-4 mx-3">
+              <h5 className="mb-3" onClick={() => toggleAccordion('Services')}>Services</h5>
+              <ul className={`list-unstyled ${isOpenServices ? 'visible' : ''}`}>
+                <li>IOT Solutions</li>
+                <li>Mobility Engineering</li>
+              </ul>
+            </div>
+            <div className="col-md-2 col-sm-6 mb-4 mx-3">
+              <h5 className="mb-3" onClick={() => toggleAccordion('Contact')}>Contact</h5>
+              <ul className={`list-unstyled ${isOpenContact ? 'visible' : ''}`}>
+                <li class="title">Phone</li>
+                <li>+91 73736 89304</li>
+                <li>+91 97898 81190</li>
+                <li class="title">Email</li>
+                <li>info@ettiksoft.com</li>
+                <li class="title">Head Office</li>
+                <li>No:6/1116-1/3, Paramathi Road, C E B Colony, Namakkal, Tamil Nadu 637001.</li>
+              </ul>
+            </div>
+         </div>
       </div>
+        
     </footer>
   );
 };
